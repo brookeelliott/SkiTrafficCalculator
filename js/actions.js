f5418d51f783a98a49297f22cc919346fd1db196
Brooke Elliott <brookeelliott711@gmail.com>
2024-01-08 16:53:28 -0700

    got api working

--- js/actions.js
+++ js/actions.js
@@ -1,36 +1,45 @@
 
 var stateInput = document.querySelector("#stateInput");
 var cityInput = document.getElementById("cityInput");
-var destinations = [ //[city, state, pass, name]
-    ["Copper Mountain", "CO", "Ikon", "Copper Mountain"],
-    ["Winter Park", "CO", "Ikon", "Winter Park"],
-    ["Jackson", "WY", "Ikon", "Jackson Hole Resort"],
-    ["Steamboat Springs", "CO", "Ikon", "Steamboat"],
-    ["Aspen", "CO", "Ikon", "Aspen Resort"],
-    ["Keystone", "CO", "Epic", "Keystone"],
-    ["Breckenridge", "CO", "Epic", "Breckenridge"],
-    ["Vail", "CO", "Epic", "Vail"],
-    ["Telluride", "CO", ,"Epic", "Telluride Resort"],
+var destinations = [ //[city state, pass, name]
+    ["Copper Mountain CO, US", "Ikon", "Copper Mountain"],
+    ["39.886426, -105.761774", "Ikon", "Winter Park"],
+    ["Jackson Wy US", "Ikon", "Jackson Hole Resort"],
+    ["Steamboat Springs CO, US", "Ikon", "Steamboat"],
+    ["Aspen CO, US", "Ikon", "Aspen Resort"],
+    ["Keystone CO, US", "Epic", "Keystone"],
+    ["Breckenridge CO, US", "Epic", "Breckenridge"],
+    ["Vail CO, US", "Epic", "Vail"],
+    ["Telluride CO, US","Epic", "Telluride Resort"],
 
 ];
 
-
+// https://api.distancematrix.ai/maps/api/distancematrix/json?origins=51.4822656,-0.1933769&destinations=51.4994794,-0.1269979&key=${API_Key}
 //Contains api requests and sorting of return data
 function validation() {
-   let data = dataGather(); //Output: [city, state, pass, travel]
-
-   let API_Key = "AIzaSyBlitlZvj1sHodzk5jiXdXKGbMiCiYXU4k";
    
-   // URL using imbedded user data and the list of ikon resorts
-   // let ikonURL = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${data[0]}%2C${data[1]}&destinations=${destinations[0][0]}%2C${destinations[0][1]}%7C${destinations[1][0]}%2C${destinations[1][1]}%7C${destinations[2][0]}%2C${destinations[2][1]}%7C${destinations[3][0]}%2C${destinations[3][1]}%7C${destinations[4][0]}%2C${destinations[4][1]}&departure_time=now&key=${API_Key}`;
-    // URL using imbedded user data and the list of epic resorts
-    //let epicURL = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${data[0]}%2C${data[1]}&destinations=${destinations[5][0]}%2C${destinations[5][1]}%7C${destinations[6][0]}%2C${destinations[6][1]}%7C${destinations[7][0]}%2C${destinations[7][1]}%7C${destinations[8][0]}%2C${destinations[8][1]}&departure_time=now&key=${API_Key}`;
-    // URL using imbedded user data and the list of all resorts
-    //let anyURL = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${data[0]}%2C${data[1]}&destinations=${destinations[0][0]}%2C${destinations[0][1]}%7C${destinations[1][0]}%2C${destinations[1][1]}%7C${destinations[2][0]}%2C${destinations[2][1]}%7C${destinations[3][0]}%2C${destinations[3][1]}%7C${destinations[4][0]}%2C${destinations[4][1]}%7C${destinations[5][0]}%2C${destinations[5][1]}%7C${destinations[6][0]}%2C${destinations[6][1]}%7C${destinations[7][0]}%2C${destinations[7][1]}%7C${destinations[8][0]}%2C${destinations[8][1]}&departure_time=now&key=${API_Key}`;
+    let city = cityInput.value;
+    let state = stateInput.value;
 
+    if(city == "" || state == ""){
+        alert("Please fill out the entire form.");
+        return console.log("incomplete form");
+    }
+   
+    let data = dataGather(); //Output: [city, state, pass, travel]
+
+   //let API_Key = "GGWaLc3iLeeNHbbsRlv7lXeyw81Qd4c4GO3cHkVa3pAavHXXOoD0AcmnVVrxZfk3";
+   
+    fetchTravelInfo();
+
+    
 
     if(data == "Ikon") { //ikon resort API call
-        
+
+
+
+
+
     } else if (data == "Epic") { // Epic resort API call
         
     } else { // Any resort api call
@@ -45,11 +54,6 @@ function dataGather() {
     let city = cityInput.value;
     let state = stateInput.value;
     let data = [];
-
-    if(city == "" || state == ""){
-        alert("Please fill out the entire form.");
-        return console.log("incomplete form");
-    }
 
     let pass = "";
     let passIndex = document.forms[1];
@@ -73,11 +77,10 @@ console.log(city);
 console.log(state);
 console.log(pass);
 console.log(travel);
-abbrState(state);
- data[0] = city;
- data[1] = state;
- data[2] = pass;
- data[3] = travel;   
+state = abbrState(state);
+ data[0] = city + ", " + state + ", US";
+ data[1] = pass;
+ data[2] = travel;   
  console.log(data);   
  return data;  
 }
@@ -150,3 +153,62 @@ function abbrState(input1){
             } 
         }
 } 
+
+function fetchTravelInfo(){
+    let url = urlBuilder();
+    //let data = dataGather(); //Output: [city, state, pass, travel]
+    let res;
+
+    let options = {
+        method: 'GET',
+        headers: {
+            'Content-Type':
+                'application/json'
+        },
+    }
+     
+    let fetchRes = fetch(
+        url,
+        options);
+    fetchRes.then(res =>
+        res.json()).then(d => {
+            console.log(d)
+            return res;
+        })
+
+}
+
+function urlBuilder() {
+    let data = dataGather(); //Output: [city, state, pass, travel]
+    let API_Key = "GGWaLc3iLeeNHbbsRlv7lXeyw81Qd4c4GO3cHkVa3pAavHXXOoD0AcmnVVrxZfk3";
+    let origin = data[0];
+    let resorts = [];
+    let pass = data[1];
+    let url;
+
+    
+    if(pass == "Any") {
+        for(let i = 0; i < destinations.length; i++) {
+                resorts = resorts + "|" + (destinations[i][0]);
+        }
+     
+        resorts = resorts.slice(1);
+        url = `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${origin}&destinations=${resorts}&key=${API_Key}`,
+        console.log(url)
+        return url;
+    }  
+    
+    for(let i = 0; i < destinations.length; i++) {
+        if(pass == destinations[i][1]) {
+            resorts = resorts + "|" + (destinations[i][0]);
+        }
+    }
+ 
+    resorts = resorts.slice(1);
+    url = `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${origin}&destinations=${resorts}&key=${API_Key}`,
+    console.log(url)
+    return url;
+
+
+
+}
