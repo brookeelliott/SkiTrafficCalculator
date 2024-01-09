ffa7cd9dcedf287b0b57e980c500c7d6047e3eb3
Brooke Elliott <brookeelliott711@gmail.com>
2024-01-09 16:48:07 -0700

    interpreted api response into something useful... OMG tricky that was awful but I got it all into my destinations array

--- js/actions.js
+++ js/actions.js
@@ -2,54 +2,71 @@
 var stateInput = document.querySelector("#stateInput");
 var cityInput = document.getElementById("cityInput");
 var destinations = [ //[city state, pass, name]
-    ["Copper Mountain CO, US", "Ikon", "Copper Mountain"],
-    ["39.886426, -105.761774", "Ikon", "Winter Park"],
-    ["Jackson Wy US", "Ikon", "Jackson Hole Resort"],
-    ["Steamboat Springs CO, US", "Ikon", "Steamboat"],
-    ["Aspen CO, US", "Ikon", "Aspen Resort"],
-    ["Keystone CO, US", "Epic", "Keystone"],
-    ["Breckenridge CO, US", "Epic", "Breckenridge"],
-    ["Vail CO, US", "Epic", "Vail"],
-    ["Telluride CO, US","Epic", "Telluride Resort"],
+    ["39.501094, -106.141064", "Ikon", "Copper Mountain", "", ""],
+    ["39.886426, -105.761774", "Ikon", "Winter Park", "", ""],
+    ["43.586701, -110.826689", "Ikon", "Jackson Hole Resort", "", ""],
+    ["40.455552, -106.808416", "Ikon", "Steamboat", "", ""],
+    ["39.186963, -106.818610", "Ikon", "Aspen Resort", "", ""],
+    ["39.605820, -105.951592", "Epic", "Keystone", "", ""],
+    ["39.486158, -106.047807", "Epic", "Breckenridge", "", ""],
+    ["39.644908, -106.388780", "Epic", "Vail", "", ""],
+    ["37.935939, -107.813387", "Epic", "Telluride Resort", "", ""],
 
 ];
 
-// https://api.distancematrix.ai/maps/api/distancematrix/json?origins=51.4822656,-0.1933769&destinations=51.4994794,-0.1269979&key=${API_Key}
-//Contains api requests and sorting of return data
-function validation() {
-   
-    let city = cityInput.value;
-    let state = stateInput.value;
 
-    if(city == "" || state == ""){
+async function validation() { //main function
+   
+    let city = cityInput.value; //Used in validation
+    let state = stateInput.value; //Used in validation
+
+    if(city == "" || state == ""){ //Handles data validation, if someone doesnt fill in the form and submits, the process ends and the api is never called
         alert("Please fill out the entire form.");
         return console.log("incomplete form");
     }
    
-    let data = dataGather(); //Output: [city, state, pass, travel]
+    let data = dataGather(); //Output: [city + state, pass, travel]
 
-   //let API_Key = "GGWaLc3iLeeNHbbsRlv7lXeyw81Qd4c4GO3cHkVa3pAavHXXOoD0AcmnVVrxZfk3";
-   
-    fetchTravelInfo();
+    //Uses the url from urlbuilder to hit the distancematrix.ai endpoint, returning an array of distances and times. 
+        //Async (as well as validates) to ensure promise finishes before call completes)
+    async function fetchTravelInfo(){
+        let url = urlBuilder();
+        let res;
+        //let travelArray = [];
 
+        let options = {
+            method: 'GET',
+            headers: {
+                'Content-Type':
+                    'application/json'
+            },
+        }
+     
+        let fetchRes = await fetch(
+            url,
+            options);
+            let travelArray = await fetchRes.json()
+            console.log(travelArray);
+            return travelArray.rows[0].elements;
+        } //DO NOT DELETE
+        
+    let travelArray = await fetchTravelInfo(); //gets data from distance matrix api
     
-
-    if(data == "Ikon") { //ikon resort API call
-
-
-
-
-
-    } else if (data == "Epic") { // Epic resort API call
-        
-    } else { // Any resort api call
-        
+    //Pulls trip distance (in meters) and duration text and puts it in the destinations array
+    for(let i = 0; i < travelArray.length; i++){
+        destinations[i][3] = travelArray[i].distance.text;
+        destinations[i][4] = travelArray[i].duration.text;
     }
+    console.log(destinations);
+
+
+
+
+
 }
 
 
-//Function that gathers data from the form and outputs [city, state, pass, travel]
-    //This function calls the abbrState function, converting state in the process. 
+//Function that gathers data from the form and outputs [city + state, pass, travel]
 function dataGather() {
     let city = cityInput.value;
     let state = stateInput.value;
@@ -57,6 +74,7 @@ function dataGather() {
 
     let pass = "";
     let passIndex = document.forms[1];
+
     for(let i = 0; i < passIndex.length; i++)
     {
         if(passIndex[i].checked) {
@@ -84,7 +102,7 @@ state = abbrState(state);
  console.log(data);   
  return data;  
 }
-//Function to convert all state inputs to their ISO code and validate for invalid inputs
+//Function to convert all state inputs to their ISO code
 function abbrState(input1){
     let input = input1;
     var states = [
@@ -153,47 +171,30 @@ function abbrState(input1){
             } 
         }
 } 
-
-function fetchTravelInfo(){
-    let url = urlBuilder();
-    //let data = dataGather(); //Output: [city, state, pass, travel]
-    let res;
-
-    let options = {
-        method: 'GET',
-        headers: {
-            'Content-Type':
-                'application/json'
-        },
-    }
-     
-    let fetchRes = fetch(
-        url,
-        options);
-    fetchRes.then(res =>
-        res.json()).then(d => {
-            console.log(d)
-            return res;
-        })
-
-}
-
+//formats the requst url to include all origin and destination information, returning a "url" to be passed into the fetch
 function urlBuilder() {
-    let data = dataGather(); //Output: [city, state, pass, travel]
+    let data = dataGather(); //Output: [city + state, pass, travel]
     let API_Key = "GGWaLc3iLeeNHbbsRlv7lXeyw81Qd4c4GO3cHkVa3pAavHXXOoD0AcmnVVrxZfk3";
     let origin = data[0];
     let resorts = [];
     let pass = data[1];
+    let travel = data[2]; //sets the distance unit for the if conditional below
     let url;
+    let unit;
 
-    
-    if(pass == "Any") {
+    if(travel == "Miles") { //sets the unit to be passed into the url and returned into the destination array
+        unit = "imperial"
+    } else if (travel == "Kilometers"){
+        unit = "metric"
+    }
+ 
+    if(pass == "Any") { //builds a string to set the destinations in the below url's
         for(let i = 0; i < destinations.length; i++) {
                 resorts = resorts + "|" + (destinations[i][0]);
         }
      
         resorts = resorts.slice(1);
-        url = `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${origin}&destinations=${resorts}&key=${API_Key}`,
+        url = `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${origin}&destinations=${resorts}&units=${unit}&key=${API_Key}`,
         console.log(url)
         return url;
     }  
@@ -205,7 +206,7 @@ function urlBuilder() {
     }
  
     resorts = resorts.slice(1);
-    url = `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${origin}&destinations=${resorts}&key=${API_Key}`,
+    url = `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${origin}&destinations=${resorts}&units=${unit}&key=${API_Key}`,
     console.log(url)
     return url;
 
